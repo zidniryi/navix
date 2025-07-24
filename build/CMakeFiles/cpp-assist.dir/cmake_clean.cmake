c3c205d3ab0292ef7647cea844b4778bf5ff2fe7
zidniryi <zidnirydwan@gmail.com>
2025-07-24 23:45:25 +0700

    basic with search

---
+++ build/CMakeFiles/cpp-assist.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp-assist.dir/src/FileScanner.cpp.o"
+  "CMakeFiles/cpp-assist.dir/src/FileScanner.cpp.o.d"
+  "CMakeFiles/cpp-assist.dir/src/main.cpp.o"
+  "CMakeFiles/cpp-assist.dir/src/main.cpp.o.d"
+  "cpp-assist"
+  "cpp-assist.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp-assist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cpp-assist.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cpp-assist.
+# This may be replaced when dependencies are built.
